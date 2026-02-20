b462db7681245e21991f9af94ad49f4caedd0065
Aditya Jha <ajha4436@gmail.com>
2026-02-21 04:05:19 +0530

    made it work somehow ig

--- main.js
+++ main.js
@@ -16,22 +16,24 @@ const sizes = {
 
 // Physics stuff
 const GRAVITY = 30;
-const CAPSULE_RADIUS = 0.35;
-const CAPSULE_HEIGHT = 1;
-const JUMP_HEIGHT = 15;
-const MOVE_SPEED = 1;
+const CAPSULE_RADIUS = 0.5;
+const CAPSULE_HEIGHT = 8;
+const JUMP_HEIGHT = 25;
+const MOVE_SPEED = 25;
 
 let character = {
     instance: null,
     isMoving: false,
+    spawnPosition: new THREE.Vector3()
 };
 
 const colliderOctree = new Octree();
-const playerController = new Capsule(new THREE.Vector3(0, CAPSULE_RADIUS, 0),
+const playerCollider = new Capsule(new THREE.Vector3(0, CAPSULE_RADIUS, 0),
                                      new THREE.Vector3(0, CAPSULE_HEIGHT, 0),
                                      CAPSULE_RADIUS
 );
 
+let playerVelocity = new THREE.Vector3();
 let playerOnFloor = false;
 
 let targetRotationY = Math.PI / 2; // Default rotation (facing left)a
@@ -102,7 +104,7 @@ const intersectObjectsNames = [
 
 
 const loader = new GLTFLoader();
-loader.load( './portfolio_collider.glb', 
+loader.load( './portfolio_collider1.glb', 
     function ( glb ) {
         glb.scene.traverse(child => {
             if (intersectObjectsNames.includes(child.name)) {
@@ -116,7 +118,20 @@ loader.load( './portfolio_collider.glb',
             console.log(child);
 
             if(child.name === "Character") {
+                character.spawnPosition.copy(child.position)
                 character.instance = child;
+                playerCollider.start
+                .copy(child.position)
+                .add(new THREE.Vector3(0, CAPSULE_RADIUS, 0));
+
+                playerCollider.end
+                .copy(child.position)
+                .add(new THREE.Vector3(0, CAPSULE_HEIGHT, 0));
+            }
+
+            if(child.name === "Ground_Collider"){
+                colliderOctree.fromGraphNode(child);
+                child.visible = false;
             }
         });
         scene.add( glb.scene );
@@ -137,10 +152,10 @@ sun.shadow.camera.bottom = -200;
 sun.shadow.normalBias = 1.8;
 scene.add( sun );
 
-const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
-scene.add( shadowHelper );
-const helper = new THREE.DirectionalLightHelper( sun, 5 );
-scene.add( helper );
+// const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
+// scene.add( shadowHelper );
+// const helper = new THREE.DirectionalLightHelper( sun, 5 );
+// scene.add( helper );
 
 const ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
 scene.add( ambientLight );
@@ -160,6 +175,8 @@ camera.position.x = 146;
 camera.position.y = 139;
 camera.position.z = 135;
 
+const cameraOffset = new THREE.Vector3(146, 139, 135);
+
 const controls = new OrbitControls( camera, canvas );
 controls.update();
 
@@ -168,6 +185,12 @@ function onWindowResize() {
     sizes.height = window.innerHeight;
 
     camera.aspect = sizes.width / sizes.height;
+
+    camera.left = -aspect * 100 / 2;
+    camera.right = -aspect * 100 / 2;
+    camera.top =  100 / 2;
+    camera.bottom = -100 / 2;
+
     camera.updateProjectionMatrix();
 
     renderer.setSize( sizes.width, sizes.height );
@@ -184,66 +207,107 @@ function onClick() {
     if(intersectObject !== "") {
         showModal(intersectObject);
     }  
-     
 }
 
-function moveCharacter(targetPosition, targetRotationY) {
-    character.isMoving = true;
+function respawnCharacter(){
+    character.instance.position.copy(character.spawnPosition)
 
-    const t1 = gsap.timeline({
-        onComplete: () => {
+    playerCollider.start
+                .copy(character.spawnPosition)
+                .add(new THREE.Vector3(0, CAPSULE_RADIUS, 0));
+
+    playerCollider.end
+                .copy(character.spawnPosition)
+                .add(new THREE.Vector3(0, CAPSULE_HEIGHT, 0));
+
+    playerVelocity.set(0,0,0);
+    targetRotationY = Math / PI;
+    character.isMoving = false;
+}
+
+function playerCollisions(){
+    const result = colliderOctree.capsuleIntersect(playerCollider);
+    playerOnFloor = false;
+
+    if(result){
+        playerOnFloor = result.normal.y > 0;
+        playerCollider.translate(result.normal.multiplyScalar(result.depth));
+
+        if(playerOnFloor){
             character.isMoving = false;
+            playerVelocity.x = 0;
+            playerVelocity.z = 0;
         }
-    });
-    t1.to(character.instance.position, {
-        x: targetPosition.x,
-        z: targetPosition.z,
-        duration: character.moveDuration,
-    });
-    t1.to(character.instance.position, {
-        x: targetPosition.x,
-        y: character.instance.position.y + character.jumpHeight,
-        yoyo: true,
-        repeat: 1,
-        z: targetPosition.z,
-        duration: character.moveDuration,
-    });
-    t1.to(character.instance.rotation, {
-        y: targetRotationY,
-        duration: character.moveDuration,
-    }, 0);
+
+    }
+}
+
+function updatePlayer(){
+    if (!character.instance) {
+        return;
+    }
+    if(character.instance.position.y < -10){
+        respawnCharacter();
+        return;
+    }
+    if(!playerOnFloor){
+        playerVelocity.y -= GRAVITY * 0.05;
+    }
+
+    playerCollider.translate(playerVelocity.clone().multiplyScalar(0.01));
+
+    playerCollisions();
+
+    character.instance.position.y -= CAPSULE_RADIUS;
+    character.instance.position.copy(playerCollider.start);
+
+    let rotationDiff = (((targetRotationY - character.instance.rotation.y) % (2 * Math.PI) +
+        3 * Math.PI) %
+        (2 * Math.PI)) - Math.PI;
+    let finalRotation = character.instance.rotation.y + rotationDiff;
+
+    character.instance.rotation.y = THREE.MathUtils.lerp(
+        character.instance.rotation.y,
+        finalRotation,
+        0.1
+    )
+
 }
 
 function onKeyDown(event) {
+    if(event.key.toLowerCase() === "r"){
+        respawnCharacter();
+        return;
+    }
     if(character.isMoving) return; // Prevent new movement if already moving
 
-    const targetPosition = new THREE.Vector3().copy(character.instance.position);
     console.log(event.key);
     switch(event.key.toLowerCase()) {
         case "d":
         case "arrowright":
-            targetPosition.z -= character.moveDistane;
+            playerVelocity.z -= MOVE_SPEED;
             targetRotationY = Math.PI; // Face right
             break;
         case "a":
         case "arrowleft":
-            targetPosition.z += character.moveDistane;
+            playerVelocity.z += MOVE_SPEED;
             targetRotationY = 0; // Face left
             break;
         case "w":
         case "arrowup":
-            targetPosition.x -= character.moveDistane;
+            playerVelocity.x -= MOVE_SPEED;
             targetRotationY = -Math.PI / 2; // Face forward
             break;
         case "s":
         case "arrowdown":
-            targetPosition.x += character.moveDistane;
+            playerVelocity.x += MOVE_SPEED;
             targetRotationY = Math.PI / 2; // Face backward
             break;
         default:
             return; // Exit if it's not a movement key
     }
-    moveCharacter(targetPosition, targetRotationY);
+    playerVelocity.y = JUMP_HEIGHT;
+    character.isMoving = true;
 }
 window.addEventListener('resize', onWindowResize);
 window.addEventListener( 'pointermove', onPointerMove );
@@ -252,7 +316,13 @@ window.addEventListener('keydown', onKeyDown);
 
 
 function onAnimate() {    
+    updatePlayer();
     //console.log(camera.position);
+    if(character.instance){
+        camera.position.copy(character.instance.position).add(cameraOffset);
+        camera.lookAt(character.instance.position);
+    }
+
     raycaster.setFromCamera( pointer, camera );
 
     const intersects = raycaster.intersectObjects( intersectObjects ,true);
